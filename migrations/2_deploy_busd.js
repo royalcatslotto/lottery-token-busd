c56c0dc0eec43ff6d46084d314061e4945663f14
Thanwa J <thanwa.jdr@gmail.com>
2021-04-11 14:51:05 +0700

    Use BigNumber

--- migrations/2_deploy_busd.js
+++ migrations/2_deploy_busd.js
@@ -1,9 +1,10 @@
 const BUSD = artifacts.require('BUSD');
+const Web3 = require('web3');
 
 module.exports = async function (deployer, network, addresses) {
   // Use deployer to state migration tasks.
-  const [alice, bob] = addresses;
+  const [alice] = addresses;
   const decimal = 18;
-  const initSupply = 1000000000;
-  await deployer.deploy(BUSD, 'Mocked Binance USD', 'BUSD', decimal, initSupply * 10 ** decimal, { from: alice });
+  const initSupply = Web3.utils.toWei('1000000000', 'ether');
+  await deployer.deploy(BUSD, 'Mocked Binance USD', 'BUSD', decimal, initSupply, { from: alice });
 };
