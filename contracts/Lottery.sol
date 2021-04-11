bc412f92698a1bb1f205d104e0805612e0dcc502
Thanwa J <thanwa.jdr@gmail.com>
2021-04-11 22:52:23 +0700

    Fix bug internal buy

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -198,7 +198,7 @@ contract Lottery is LotteryOwnable, Initializable {
     function internalBuy(uint256 _price, uint8[4] memory _numbers) internal {
         require(!drawed(), "drawed, can not buy now");
         for (uint256 i = 0; i < 4; i++) {
-            require(_numbers[i] == maxNumber, "exceed the maximum");
+            require(_numbers[i] <= maxNumber, "exceed the maximum");
         }
         uint256 tokenId =
             lotteryNFT.newLotteryItem(
