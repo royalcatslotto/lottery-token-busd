fa58cb04a4b4ee4176e077859c7e30c9618558ec
Suraneti Rodsuwan <suraneti.rod@gmail.com>
2021-05-01 14:43:25 +0700

    Update schedule time

--- scripts/auto-script.js
+++ scripts/auto-script.js
@@ -121,9 +121,8 @@ function getUserAddress(privateKey) {
   }
 }
 
-const DRAWING_SCHEDULE = [14, 20] // run every 14.00 pm, 20.00 pm (UTC time)
 const rule = new schedule.RecurrenceRule();
-rule.hour = DRAWING_SCHEDULE;
+rule.hour = 2; // run every 2.00 am (UTC time)
 rule.tz = 'Etc/UTC';
 
 async function main() {
