f6c98d399f98b36d9c085ac61e9bf078eada8e12
korrio <korr@aq1.co>
2021-05-18 20:21:36 +0700

    finializing auto-script

--- scripts/auto-script.js
+++ scripts/auto-script.js
@@ -129,17 +129,55 @@ function getUserAddress(privateKey) {
   }
 }
 
-const DRAWING_SCHEDULE = [2, 14] // run every 9.00 am, 9.00 pm (GMT+7 time)
+// const DRAWING_SCHEDULE = [2, 14] // run every 9.00 am, 9.00 pm (GMT+7 time)
+// const drawingRule = new schedule.RecurrenceRule();
+// drawingRule.hour = DRAWING_SCHEDULE;
+// drawingRule.minute = 0;
+// drawingRule.tz = 'Etc/UTC';
+
+// const RESET_SCHEDULE = [3, 15] // run every 10.00 am, 10.00 pm (GMT+7 time)
+// const resetRule = new schedule.RecurrenceRule();
+// resetRule.hour = RESET_SCHEDULE;
+// resetRule.minute = 0;
+// resetRule.tz = 'Etc/UTC';
+
+const DRAWING_SCHEDULE = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+const DRAWING_SCHEDULE_MIN = [10,25,40,55]; 
+const RESET_SCHEDULE_MIN = [0,15,30,45]; 
 const drawingRule = new schedule.RecurrenceRule();
 drawingRule.hour = DRAWING_SCHEDULE;
-drawingRule.minute = 0;
+drawingRule.minute = DRAWING_SCHEDULE_MIN;
 drawingRule.tz = 'Etc/UTC';
 
-const RESET_SCHEDULE = [3, 15] // run every 10.00 am, 10.00 pm (GMT+7 time)
 const resetRule = new schedule.RecurrenceRule();
-resetRule.hour = RESET_SCHEDULE;
-resetRule.minute = 0;
+resetRule.hour = DRAWING_SCHEDULE;
+resetRule.minute = RESET_SCHEDULE_MIN;
 resetRule.tz = 'Etc/UTC';
+
+async function main_test() {
+  while (1) {
+    await sleep(10000);
+    const time = Date.parse(new Date()) / 1000;
+    if ((time - 240) % 300 < 18) { // draw every 5 minutes
+      try {
+        await reset(alice); // reset @ 17:14:02, 17:19:01, 17:24:01, 17:29:02
+      }
+      catch (err) {
+        console.error(err);
+      }
+    }
+
+    else if ((time - 180) % 300 < 18) {
+      try {
+        await enterDrawing(alice); // 17:18:09, 17:23.08, 17:28:09
+        await drawing(alice); // 17.18.21, 17:23:21, 18:28:22
+      }
+      catch (err) {
+        console.error(err);
+      }
+    }
+  }
+}
 
 async function main() {
   console.log('Scheduler starting')
@@ -174,31 +212,9 @@ async function main() {
     }
   })
 
-  // while (1) {
-  //   console.log("Initializing...");
-  //   await sleep(10000);
-  //   console.log("Started counting...");
-  //   const time = Date.parse(new Date()) / 1000;
-  //   if ((time - 240) % 300 < 18) { // draw every 5 minutes
-  //     try {
-  //       await reset(alice); // reset @ 17:14:02, 17:19:01, 17:24:01, 17:29:02
-  //     }
-  //     catch (err) {
-  //       console.error(err);
-  //     }
-  //   }
-
-  //   else if ((time - 180) % 300 < 18) {
-  //     try {
-  //       await enterDrawing(alice); // 17:18:09, 17:23.08, 17:28:09
-  //       await drawing(alice); // 17.18.21, 17:23:21, 18:28:22
-  //     }
-  //     catch (err) {
-  //       console.error(err);
-  //     }
-  //   }
-  // }
+  
 }
 
-main();
+//main();
+main_test();
 
